5048375cb616037c6cb8947ed6ca3d12d5e078ea
Marwa Jawad <marwajawad@Marwas-MacBook-Pro.local>
2024-11-13 15:46:07 +0400

    add edit function

--- src/Redux/toDoListReducer.js
+++ src/Redux/toDoListReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TASK, DELETE_TASK } from "./actionsTypes";
+import { ADD_TASK, DELETE_TASK, EDIT_TASK } from "./actionsTypes";
 
 const initialState = {
   tasks: [],
@@ -16,6 +16,20 @@ const taskReducer = (state = initialState, action) => {
       return {
         ...state,
         tasks: state.tasks.filter((task) => task.id !== action.payload),
+      };
+
+    case EDIT_TASK:
+      let { id, text } = action.payload;
+
+      return {
+        ...state,
+        tasks: state.tasks.map((task) => {
+          if (id == task.id) {
+            return { ...task, text: text };
+          } else {
+            return task;
+          }
+        }),
       };
     default:
       return state;
